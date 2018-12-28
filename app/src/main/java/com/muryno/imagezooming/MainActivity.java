1acac33d2a2352c316dc719366efddf489c58420
muryno <github.com/muryno>
2018-12-28 07:34:45 +0100

    Initial commit

---
+++ app/src/main/java/com/muryno/imagezooming/MainActivity.java
@@ -0,0 +1,18 @@
+package com.muryno.imagezooming;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.muryno.imagezooming.zoomImage.PhotoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        PhotoView photoView = findViewById(R.id.iv);
+        photoView.setImageResource(R.drawable.image);
+    }
+}
